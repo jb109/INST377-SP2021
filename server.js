3c4cd65a1b007f06f4061afcdcb7bc6e994f45d8
jhg <jbbillion@yahoo.com>
2021-02-20 00:31:37 -0500

    submit box

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // These are our required libraries to make the server work.
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import fetch from 'node-fetch';
-import reload from 'livereload';
-import connectReload from 'connect-livereload';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+import reload from "livereload";
+import connectReload from "connect-livereload";
 
 dotenv.config();
 
 const __dirname = path.resolve();
 const app = express();
 const port = process.env.PORT || 3000;
-const staticFolder = 'public';
+const staticFolder = "public";
 
 // Add some auto-reloading to our server
 const liveReloadServer = reload.createServer();
@@ -26,32 +26,39 @@ app.use(express.json());
 app.use(express.static(staticFolder));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.route('/api')
+app
+  .route("/api")
   .get(async (req, res) => {
-    console.log('GET request detected');
-    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    console.log("GET request detected");
+    const data = await fetch(
+      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
+    );
     const json = await data.json();
-    console.log('data from fetch', json);
+    console.log("data from fetch", json);
     res.json(json);
   })
   .post(async (req, res) => {
-    console.log('POST request detected');
-    console.log('Form data in res.body', req.body);
-    console.log('Now send something back to your client');
+    console.log("POST request detected");
+    console.log("Form data in res.body", req.body);
+    console.log("Now send something back to your client");
     // res.json({data: dataToSendToFrontEnd});
+    res.send("hello world");
   });
 
 app.listen(port, async () => {
   console.log(`Example app listening on port ${port}!`);
 });
 
-liveReloadServer.server.once('connection', () => {
+liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
-    liveReloadServer.refresh('/');
+    liveReloadServer.refresh("/");
   }, 100);
 });
